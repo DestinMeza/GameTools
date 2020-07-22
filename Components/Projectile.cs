92c55d1b08bbe049495aa2a2a2b7a0a941ba4f13
Destin Meza <desti@Maple.DESKTOP-J4NQENH>
2020-07-22 12:57:31 -0500

    Added projectile launcher and projectile scripts.

---
+++ Components/Projectile.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameTools.Components
+{
+    public class Projectile : MonoBehaviour
+    {
+        public float lifeTime = 1;
+        public float maxSpeedChange = 1;
+        public bool dieOffScreen = false;
+        public bool dieOnCollision = true; 
+        Rigidbody2D rb2D;
+        Rigidbody rb;
+        Vector3 targetVel;
+        Vector2 targetVel2D;
+        public float speed;
+        bool launched = false;
+        float lastLaunchTime = 0;
+        public string[] ignoreTags = new string[1] {"Ignore"};
+        Camera cam;
+        void Awake(){
+            cam = FindObjectOfType<Camera>();
+            rb = GetComponentInParent<Rigidbody>();
+            rb2D = GetComponentInParent<Rigidbody2D>();
+        }
+        void Update(){
+            if(dieOffScreen){
+                Vector3 pos = cam.WorldToViewportPoint(transform.position);
+                if(pos.x > 1 || pos.x < 0 || pos.y < 0 || pos.y > 1) gameObject.SetActive(false);
+            }
+            if(Time.time - lastLaunchTime > lifeTime){
+                gameObject.SetActive(false);
+            }
+        }
+
+        void OnDisable(){
+            launched = false;
+        }
+
+        void FixedUpdate()
+        {
+            if(launched){
+                if(rb != null) rb.velocity = targetVel.normalized * speed;
+                else{
+                    rb2D.velocity = targetVel2D.normalized * speed;
+                }
+            }
+
+        }
+
+        public void Shoot(Vector3 dir){
+            if(rb != null){
+                targetVel = dir;
+            }
+            else{
+                targetVel2D = dir;
+            }
+            lastLaunchTime = Time.time;
+            launched = true;
+        }
+
+        void OnCollisionEnter(Collision col){
+            foreach(string tag in ignoreTags){
+                if(gameObject.CompareTag(tag)){
+                    return;
+                }
+            }
+            if(dieOnCollision)gameObject.SetActive(false);
+        }
+
+        void OnCollisionEnter2D(Collision2D col){
+            foreach(string tag in ignoreTags){
+                if(gameObject.CompareTag(tag)){
+                    return;
+                }
+            }
+            if(dieOnCollision)gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Components/ProjectileLauncher.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameTools.Components
+{
+    public class ProjectileLauncher : MonoBehaviour
+    {
+        public delegate void OnReload();
+        public OnReload onReload = delegate{};
+        public string bulletName = "";
+        public bool inheritVel = true;
+        public int ammoDecay = 1;
+        public int maxAmmo = 10;
+        public float fireRate = 1;
+        public float reloadTime = 5;
+        public float accuracy = 1;
+        public bool useReload = false; 
+        public ForceMode forceType;
+        public ForceMode2D forceType2D;
+        int ammo = 10;
+        float lastShotTime = -1;
+        float lastReloadTime = -1;
+        bool reloading = false;
+        Rigidbody rb;
+        Rigidbody2D rb2D;
+        void Awake(){
+            rb = GetComponentInParent<Rigidbody>();
+            if(rb == null) rb2D = GetComponentInParent<Rigidbody2D>();
+
+            if(rb == null && rb2D == null) useReload = false;
+        }
+
+        public void Fire(Vector3 targetDir){
+            
+            if(Time.time - lastReloadTime > reloadTime){
+                if(reloading){
+                    onReload();
+                    ammo = maxAmmo;
+                    reloading = false;
+                }
+
+                if(Time.time - lastShotTime > fireRate){
+                    GameObject bullet = SpawnManager.Spawn(bulletName, transform.position);
+                    Projectile bulletBody = bullet.GetComponentInParent<Projectile>();
+                    if(bulletBody != null){
+                        bullet.SetActive(true);
+                        lastShotTime = Time.time;
+                        Vector3 actualDir = Quaternion.AngleAxis(180 * Random.Range(accuracy-1, 1-accuracy), Vector3.up) * targetDir;
+                        actualDir += (Quaternion.AngleAxis(180 * Random.Range(accuracy-1, 1-accuracy), Vector3.right) * targetDir);
+                        bulletBody.Shoot(actualDir);
+                        if(useReload) ammo -= ammoDecay;
+                    }
+                }
+
+                if(ammo <= 0){
+                    lastReloadTime = Time.time;
+                    reloading = true;
+                }
+            }
+        }
+    }
+}
+
