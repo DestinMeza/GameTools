fd7afc505a5254d9db13485da0d6dade4156fb34
Destin Meza <dmeza@dmi.edu>
2020-08-20 11:36:05 -0500

    Added Texture Gen

--- Editor/TextureGenerator.cs
+++ Editor/TextureGenerator.cs
@@ -7,12 +7,20 @@ using System.IO;
 namespace GameTools.Editor{
     public class TextureGenerator : ScriptableWizard{
 
+        public string name = "texture";
+
         public int width = 1024;
         public int height = 1024;
+
         public float scale = 1;
-        public string name = "texture";
+        
         public float xOffset = 0;
         public float yOffset = 0;
+
+        float _scale; 
+        float _xOffset = 0;
+        float _yOffset = 0;
+
         Texture2D texture;
         Color[] colors;
 
@@ -25,12 +33,21 @@ namespace GameTools.Editor{
         void OnWizardCreate(){
             UpdateTexture();
             byte[] bytes = texture.EncodeToPNG();
-            string path = EditorUtility.SaveFilePanelInProject("Save png", "texture", "png", "Saving File?");
+            string path = EditorUtility.SaveFilePanelInProject("Save png", name, "png", "Saving File?");
             File.WriteAllBytes(path, bytes);
             AssetDatabase.Refresh();
         }
 
+        void OnWizardUpdate(){
+            if(texture == null || texture.width != width || texture.height != height || scale != _scale || xOffset != _xOffset || yOffset != _yOffset){
+                UpdateTexture();
+            }
+        }
+
         void UpdateTexture(){
+            _scale = scale;
+            _xOffset = xOffset;
+            _yOffset = yOffset;
             texture = new Texture2D(width, height);
             colors = new Color[width*height];
 
@@ -46,6 +63,7 @@ namespace GameTools.Editor{
             }
             texture.SetPixels(colors);
             texture.Apply();
+            
         }
     }
 }
